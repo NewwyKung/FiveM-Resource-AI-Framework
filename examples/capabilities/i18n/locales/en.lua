96df7c75e2dd943d7db747c5f694b2f427881d7a
Nat B. <90946952+NewwyKung@users.noreply.github.com>
2026-08-05 03:30:55 +0700

    feat: add optional i18n locale examples

---
+++ examples/capabilities/i18n/locales/en.lua
@@ -0,0 +1,8 @@
+return {
+    locale = 'en',
+    messages = {
+        ['common.close'] = 'Close',
+        ['common.confirm'] = 'Confirm',
+        ['error.unknown'] = 'Something went wrong.',
+    },
+}
