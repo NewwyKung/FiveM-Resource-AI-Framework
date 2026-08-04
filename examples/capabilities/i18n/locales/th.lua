16e6b3a50d8c504a7700c2c6e55dde45d3cfce67
Nat B. <90946952+NewwyKung@users.noreply.github.com>
2026-08-05 03:31:05 +0700

    feat: add optional Thai locale example

---
+++ examples/capabilities/i18n/locales/th.lua
@@ -0,0 +1,8 @@
+return {
+    locale = 'th',
+    messages = {
+        ['common.close'] = 'ปิด',
+        ['common.confirm'] = 'ยืนยัน',
+        ['error.unknown'] = 'เกิดข้อผิดพลาด กรุณาลองใหม่อีกครั้ง',
+    },
+}
